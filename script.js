f6f36046be9956fe6721c291c4a53e7b3cd74e58
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-20 23:28:02 +0500

    code is added

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ function getValues() {
     count = inputNum.value;
     options = inputOpt.value;
     // console.log(count, options);
-    url = `https://baconipsum.com/api/?type=meat-and-filler&${options}=${count}&format=text`
-    
+    url = `https://baconipsum.com/api/?type=meat-and-filler&${options}=${count}&start-with-lorem=1`
+    fetchCounter(url)
 }
+
+async function fetchCounter(Url) {
+    let response = await fetch(Url)
+    if(response.status === 200){
+        let data = await response.json();
+        console.log(data);
+        
+    }
+
+}
+
+// NOTE:
+
+// async wait promise return krta hai and jb async use hota hai tu hum await ka use krty hain or jahan hum await lagaty hain oska
+// mtlb ye hota hai k jahan m await lgya hai tm wahan pr stop na kro kisi network error ya kisi b waja sy balky data fetch kro jahan await laga or next line pr jao yahan pr e stop na kro...
