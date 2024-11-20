9e2d312efee414032bfa157b09f060ef3ef6e7ea
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-20 22:38:45 +0500

    code is added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+const inputNum = document.querySelector("#num-input")
+const inputOpt = document.querySelector("#opt-input")
+const genBtn = document.querySelector("#gen-btn")
+const copyBtn = document.querySelector("#copy-btn")
